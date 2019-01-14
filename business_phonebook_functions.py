78ee2d537dfb06cb7732854eb5b3be547eb332c4
Rachel Winkler <rachelwinkler9@gmail.com>
2019-01-14 16:42:33 +0000

    added comments to explain code

--- business_phonebook_functions.py
+++ business_phonebook_functions.py
@@ -6,8 +6,12 @@ Created on Mon Jan 14 16:03:28 2019
 """
 from phonebook_database import *
 
-business_category_list = []
+#---------------------------------------------#
+#Filtering Business Table by Business Category
+#---------------------------------------------#
 
+business_category_list = []
+##---creates list of businesses based on user input---###
 def create_business_category_list():
     c.execute('SELECT * FROM business_table')
     for row in c.fetchall():
@@ -16,18 +20,16 @@ def create_business_category_list():
     return business_category_list
 create_business_category_list()
 
-
+###---returns business types which match user's search---###
 def extract_business_type_list(user_category):
     c.execute('SELECT * FROM business_table WHERE business_category =?', (user_category,))
     for row in c.fetchall():
         print(row)
     return(row)
-        
-    
 
+###---user chooses which business tye to filter results by---###        
 def sort_business_type():
     user_category = input('Choose one of the following business types{}'.format(business_category_list))
-
     extract_business_type_list(user_category)
         
 sort_business_type()
